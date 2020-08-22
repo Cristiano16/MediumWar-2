73f0fce3b923d4a3b15b3bf398fc18e9eaf0c8cb
Felipe <fpachecomanoel@uol.com.br>
2020-08-21 21:44:33 -0300

    input right e inicio do jogador

--- Godot/Scripts/Player.gd
+++ Godot/Scripts/Player.gd
@@ -1,5 +1,9 @@
 extends Node2D
 var mao=[[null,null,null],[null,null,null],[null,null,null]]
-
+export var id=0
 func _ready():
 	pass
+	
+func _process(delta):
+	if Input.is_action_just_released("right%s"%id):
+		pass
